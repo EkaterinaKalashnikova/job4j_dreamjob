bd8c83b74adf5bb4e861f8bff97a99f8b9f5bdd9
Ekaterina Kalashnikova <ladykati73@gmail.com>
2022-04-02 16:27:08 +0300

    3. Bootstrap, Table [#504840]

--- src/main/java/ru/job4j/dream/controller/LessonControl.java
+++ src/main/java/ru/job4j/dream/controller/LessonControl.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LeccionControl {
-    @GetMapping("/leccion")
+public class LessonControl {
+    @GetMapping("/lesson")
     public String leccion() {
-        return "leccion";
+        return "lesson";
     }
 }
