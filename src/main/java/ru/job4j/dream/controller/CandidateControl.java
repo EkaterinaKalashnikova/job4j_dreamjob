1db29d26ca939f80ade3882268d089204bc80771
Ekaterina Kalashnikova <ladykati73@gmail.com>
2022-04-17 20:59:59 +0300

    3. Формы. Загрузка файла на сервер. [#504855 #290089]

--- src/main/java/ru/job4j/dream/controller/CandidateControl.java
+++ src/main/java/ru/job4j/dream/controller/CandidateControl.java
@@ -49,7 +49,9 @@ public class CandidateControl {
     }
 
     @PostMapping("/updateCandidate")
-    public String updateCandidate(@ModelAttribute Candidate candidate) {
+    public String updateCandidate(@ModelAttribute Candidate candidate,
+                                  @RequestParam("file") MultipartFile file) throws IOException {
+        candidate.setPhoto(file.getBytes());
         candidateService.update(candidate);
         return "redirect:/candidates";
     }
