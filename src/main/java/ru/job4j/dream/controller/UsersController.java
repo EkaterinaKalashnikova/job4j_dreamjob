6d0d5108b542902e6a02652d1dd6f91d0a77890c
Ekaterina Kalashnikova <ladykati73@gmail.com>
2022-07-25 21:11:59 +0300

    1. Страница login.html [#504863 #318819]

--- src/main/java/ru/job4j/dream/controller/UsersController.java
+++ src/main/java/ru/job4j/dream/controller/UsersController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.dream.model.User;
 import ru.job4j.dream.service.UserService;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -44,21 +46,46 @@ public class UsersController {
         return "registration";
     }
 
-    @GetMapping("/loginPage")
-    public String loginPage(Model model, @RequestParam(name = "fail", required = false) Boolean fail) {
+    @GetMapping("/registrationPage")
+    public String registrationPage(Model model, HttpSession session, @RequestParam(name = "fail", required = false) Boolean fail) {
         model.addAttribute("fail", fail != null);
+        sessionAll(model, session);
+        return "registration";
+    }
+
+    private void sessionAll(Model model, HttpSession session) {
+    }
+
+    @GetMapping("/loginPage")
+    public String loginPage(Model model, HttpSession session, @RequestParam(name = "fail", required = false) Boolean fail) {
+        model.addAttribute("fail", fail != null);
+        sessionAll(model, session);
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute User user) {
+    public String login(@ModelAttribute User user, HttpServletRequest req) {
         Optional<User> userDb = userService.findUserByEmailAndPassword(
                 user.getEmail(), user.getPassword()
         );
         if (userDb.isEmpty()) {
             return "redirect:/loginPage?fail=true";
         }
+        HttpSession session = req.getSession();
+        session.setAttribute("user", userDb.get());
         return "redirect:/index";
     }
-}
 
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/loginPage";
+    }
+}
+/*1. Доработайте информацию о текущем пользователе в HttpSession.
+
+2. Добавьте вывод текущего пользователя на все html страницы в шапке.
+
+3. Добавьте кнопку выхода из аккаунта на страницы.
+
+ */
